275013c931943084460a8d67c27a7e1e0c2ebaa6
ggberry <51100020+ggberry@users.noreply.github.com>
2025-08-28 19:12:52 -0400

    Added favicon, updated UI and more

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask
+import os
+from flask import Flask, url_for, send_from_directory
 
 from pages.index import index_page
 
 app = Flask(__name__)
 app.register_blueprint(index_page)
 
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- pages/index.py
+++ pages/index.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
 
 index_page = Blueprint('login_page', __name__)
 
-@index_page.route('/', methods=['GET'])
+
+@index_page.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        print(request.form.get("class-input"))
+
     return render_template("index.html")
